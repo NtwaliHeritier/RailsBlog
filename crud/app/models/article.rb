735e6d5d1b7053a8d976b3e6207a3849e50440e2
NtwaliHeritier <heritierntwali@gmail.com>
2020-06-27 19:36:35 +0200

    added tag model

--- crud/app/models/article.rb
+++ crud/app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
+    has_many :taggings
+    has_many :tags, through: :taggings, dependent: :destroy
+    
+
+    def tag_list
+        tags.join(",")
+    end
+
+    def tag_list=(tag_names)
+        tag_name=tag_names.split(",").collect{|t| t.strip.downcase}.uniq
+        tag_return=tag_name.collect{|s| Tag.find_or_create_by(name: s)}
+        self.tags=tag_return
+    end
+
 end
